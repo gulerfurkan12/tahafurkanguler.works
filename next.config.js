9b1d0ca309a59065cceabb4f54ecc84e1d8068f9
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-11-13 21:15:03 +0300

    disable next/image

--- next.config.js
+++ next.config.js
@@ -1,14 +1,30 @@
 const path = require('path')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const withPWA = require('next-pwa');
 
-module.exports = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['dl.airtable.com']
-  },
-  env:{
-    id:'local'
-  },
+const nextConfig = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+
+  return{
+    reactStrictMode: true,
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
+    images: {
+      domains: ['dl.airtable.com']
+    },
+    env:{
+      id:'local'
+    },
+    pwa:{
+      dest:'public',
+      disable:isDev
+    },
+    
+  }
 }
+
+
+module.exports = (phase) => withPWA(nextConfig(phase))
+
+
